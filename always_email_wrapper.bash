a9ba1a4b83f9dee7d09632809506b72d7e9ac8b1
cesarlr <cesarlr@gmail.com>
2016-02-24 10:40:27 +0100

    Concatenation of log file

--- always_email_wrapper.bash
+++ always_email_wrapper.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 source $VIRTUALENV_PATH/bin/activate
-"$@" 2>&1 | emili.py -f prueba@example.com -s $SUBJ_EMAIL -t $RCPT_EMAIL >/tmp/salida_emili.txt 2>&1
+"$@" 2>&1 | emili.py -f prueba@example.com -s $SUBJ_EMAIL -t $RCPT_EMAIL >>/tmp/salida_emili.txt 2>&1
 
