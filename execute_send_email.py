b6f4cfe939631e53bc17589d7addfdfeb04c33cc
César López Ramírez <cesar.lopez@somenergia.coop>
2016-02-29 14:34:43 +0100

    First version of execute_send_email

--- execute_send_email.py
+++ execute_send_email.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python2
 # -*- encoding: utf8 -*-
-
+# Execute this: ./execute_send_email.py -c SMTP_CONFIG.PY_PATH  -s SUBJECT -t RECIPIENT [-t RECIPIENT2] [-t RECIPIENT3] -- "pip freeze | wc -l" (Command within quotes and after "--"]
 import argparse
 import subprocess
 import os
@@ -17,6 +17,13 @@ def main():
         help="Message subject ('Subject:' header)",
         )
     parser.add_argument(
+        '-c',
+        '--config',
+        required=True,
+        dest='config',
+        help="Message subject ('Subject:' header)",
+        )
+    parser.add_argument(
         '-t',
         '--to',
         dest='to',
@@ -29,7 +36,6 @@ def main():
 
     args = parser.parse_args()
     command = " ".join(args.command[1:])
-    os.system(command)
-
+    os.system(command+" 2>&1 | PYTHONPATH=$PYTHONPATH:"+args.config+" emili.py -f sender@example.org -s "+args.subject+" "+" ".join(["-t "+arg for arg in args.to])+" >>/tmp/output_emili.txt 2>&1")
 if __name__ == '__main__':
     main()
